0e1e9aa250a2f8b135b31291ed3cb810eb147cfe
Vitaly Medvedev <cisco.medvedev2000@gmail.com>
2023-10-10 22:54:08 +0300

    OAuth2 with Password (and hashing), Bearer with JWT tokens

--- app/api/endopoints/users.py
+++ app/api/endopoints/users.py
@@ -1,71 +1,40 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException
 from core.security import (
-    fake_hash_password,
-    oauth2_scheme,
-    OAuth2PasswordRequestForm
+    get_current_active_user,
+    OAuth2PasswordRequestForm,
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    create_access_token,
+    authenticate_user
 )
-from api.models.user import User, UserInDB
-from db import get_user
+from api.models.user import User, Token
+from datetime import timedelta
 
 Router = APIRouter()
 
 
-# @Router.get('/register')
-# async def registration_user(token: Annotated[str, Depends(oauth2_sheme)]):
-#     # Конечная точка для регистрации пользователей
-#     return {'token': token}
-
-
-# @Router.get('/login')
-# async def login_user():
-#     # Конечная точка входа для генерации токенов для аутентификации
-#     pass
-
-
-def fake_decode_token(token):
-    # просто пример (нулевой ур. безопасности)
-    user = get_user(token)
-    return user
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
+@Router.post('/token', response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)]
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail='Inactive user')
-    return current_user
-
-
-@Router.post('/token')
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    # проверка наличия пользователя в БД
-    user = get_user(form_data.username)
-    if not user:
-        raise HTTPException(status_code=400, detail='Incorrect username or password')
-    # проверка совпадения хэшей паролей введенных пользователем и сохр. в БД
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail='Incorrect username or password')
-
+            status_code=401,
+            detail='Incorrect username or password',
+            headers={'WWW-Authenticate': 'Bearer'},
+            )
+    # время истечения токена и его создание
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={'sub': user.username}, expires_delta=access_token_expires
+    )
     # ответ должен содержать поля token_type, access_token (токен доступа)
-    # сейчас небезопасно, ибо возвращается просто имя
-    return {'access_token': user.username, 'token_type': 'bearer'}
+    return {'access_token': access_token, 'token_type': 'bearer'}
 
 
 @Router.get('/me')
 async def read_users_me(
     current_user: Annotated[User, Depends(get_current_active_user)]
 ):
-    return current_user
+    return [{'item_id': 'Succesfull', 'owner': current_user.username}]
